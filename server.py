380624e3332fbd7df53f6ca31e82631360ba2d09
Jason Guo <jasonguo5855@gmail.com>
2018-02-25 14:23:46 -0600

    establish connections to other sockets on initialization and parts 1 and 2 completely done

--- server.py
+++ server.py
@@ -6,6 +6,7 @@ import random
 import sys
 import signal
 from datetime import datetime
+import select
 
 class listener_thread (threading.Thread):
     def __init__(self, queue, address_to_pid):
@@ -13,110 +14,145 @@ class listener_thread (threading.Thread):
         self.queue = queue
         self.address_to_pid = address_to_pid
     def run(self):
+        global inputs
         while(1):
-            lock.acquire()
-            # print "Waiting for incoming connections..."
-            try: 
-                connection, client_address = server.accept()
-            except socket.error, strerror:
-                lock.release()
-                continue 
-            self.queue[connection] = Queue.Queue()
-            data = ''
-            while(not data):
-                try: 
-                    data = connection.recv(1024)
-                    self.queue[connection].put(data)
-                except socket.error, strerror:
-                    continue 
-            remote_server_address = (client_address[0], client_address[1] - client_address[1]%10)
-            pid_from = self.address_to_pid[remote_server_address]
-            time = str(datetime.now())
-            print("Received \"" + data + "\" from process " + pid_from + ", system time is " + time)
-            connection.close()
-            lock.release()
+            readable, writable, exceptional = select.select(inputs, [], [], 0) 
+            for s in readable:
+                if s is server:
+                    connection, client_address = s.accept()
+                    client_pid = int(connection.recv(1024))
+                    print('new connection from ' + str(client_pid))
+                    inputs.append(connection)
+                    global pid_to_socket
+                    global address_to_pid
+                    pid_to_socket[client_pid] = connection
+                    address_to_pid[client_address] = client_pid
+                else: # case where s is a client socket 
+                    data = s.recv(1024)
+                    if data:
+                        pid_from = address_to_pid[s.getpeername()]
+                        time = str(datetime.now())
+                        print("Received \"" + data + "\" from process " + str(pid_from) + ", system time is " + time)
+                    else:
+                        print("client connection is over")
+                        s.close()
 
+                
 
 class client_thread (threading.Thread):
 
-    def __init__(self, listener, pid_to_address):
+    def __init__(self, queue, pid_to_socket):
         super(client_thread, self).__init__()
-        self.listener = listener
-        self.pid_to_address = pid_to_address
-
+        self.queue = queue
     def run(self):
+        client_init()
         while(1):
-            client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            cli_arg = raw_input("Enter command line argument for client: ")
-            lock.acquire()
-            command, destination, message = cli_arg.strip().split(' ')
-            if(command == 'send'): 
-                global client_port_offset
-                client_port_offset = (client_port_offset + 1) % 10
-                client_port = (server_binding_addr[1] + 1) if client_port_offset == 0 else (client_port_offset + server_binding_addr[1])
-                client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-                client_binding_addr = (server_binding_addr[0], client_port)
-                client.bind(client_binding_addr)
-                # print('Client addr is: ' + client_binding_addr[0] + ':' + str(client_binding_addr[1]))
-                dest = self.pid_to_address[destination]
-                # print('Destination addr is: ' + dest[0] + ':' + str(dest[1]))
-                random_delay = random.uniform((float(min_delay)/1000), (float(max_delay)/1000))
-                client.connect(dest)
-                sender_thread = threading.Timer(random_delay, unicast_send, [destination, client, message])   
-                sender_thread.start()
-                sender_thread.join()
+            raw_argument = raw_input("Enter command line argument for client: ")
+            cli_arg = raw_argument.strip().split(' ')
+            if(cli_arg[0] == 'send'): 
+                unicast_send(cli_arg)
+            elif(cli_arg[0] == 'msend' and cli_arg[2] == 'casual'):
+                casual_order_send(client, cli_arg, self.queue)
             else:
                 print("Invalid CLI argument. Please follow this format: send destination message")
-            lock.release()
 
+# establish a connection between every pair of processes 
 
-def unicast_send(destination, client, message):
-    client.send(message)
+def client_init():
+    clients = []
+    global inputs
+    for i in range(pid_count-1):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        clients.append(sock)
+    for client, pid, address in zip(clients, pid_to_address.keys(), pid_to_address.values()):        # establish a connection between every pair of processes 
+            result = client.connect_ex(address)
+            if(not result):
+                print("Client connected to process " + str(pid))
+                global pid_to_socket
+                pid_to_socket[pid] = client
+                inputs.append(client)
+                client.send(server_pid) # send process id to every other process  
+            else:
+                print("Could not connect to process " + str(pid))
+
+# unicast functions are below ----------------------------------------------------------------
+
+def tcp_send(destination, send_socket, message):
+    send_socket.send(message)
     time = str(datetime.now())
-    print("Sent \"" + message + "\" to process " + destination + ", system time is " + time)
-    client.close()
+    print("Sent \"" + message + "\" to process " + str(destination) + ", system time is " + time)
+
+def unicast_send(cli_arg):
+    destination = int(cli_arg[1])
+    message = cli_arg[2]
+    send_socket = pid_to_socket[destination]
+    random_delay = random.uniform((float(min_delay)/1000), (float(max_delay)/1000))
+    sender_thread = threading.Timer(random_delay, tcp_send, [destination, send_socket, message])   
+    sender_thread.start()
+    sender_thread.join()
+
+# multicast functions 
+
+def casual_order_send(client, cli_arg, pid_to_address, queue):
+    # for every server in pid_to_address aside from own pid, call tcp_send
+    message = cli_arg[1]
+    queue[server_binding_address].put(message)
+    pid_to_vector[server_pid][server_pid] = pid_to_vector[server_pid][server_pid]+1 # increment process i's count of messages from i 
+    for pid, address in pid_to_address.iteritems():
+        if(pid != server_pid):
+            random_delay = random.uniform((float(min_delay)/1000), (float(max_delay)/1000))
+            client.connect(address)
+            sender_thread = threading.Timer(random_delay, tcp_send, [address, client, message])   
+            sender_thread.start()
+
 
 
 def server_init():
-    global server
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server.setblocking(0)
+    # server.setblocking(0)
     server.bind(server_binding_addr)
-    server.listen(4)
+    print('Server binding to ' + str(server_binding_addr[1]))
+    server.listen(pid_count+1)
     return server 
 
 
 # set up a server for each process in the config file, and bind it to its assigned ip and port 
 message_queue = {} 
+pid_to_vector = {}
 pid_to_address = {}
 address_to_pid = {}
+pid_to_socket = {}
+socket_to_pid = {}
+pid_count = 0
+server_pid = sys.argv[1]
+
 with open("config.txt", "r") as file:
     delay = file.readline()
     for row in file:
         if(row not in ['\n', '\r\n']):
             stripped = row.strip()
             pid, ip, port = stripped.split(' ')
-            server_address = (str(ip), int(port))
-            pid_to_address[pid] = server_address
-            address_to_pid[server_address] = pid
+            if(pid != server_pid): 
+                pid = int(pid)
+                client_address = (str(ip), int(port))
+                pid_to_address[pid] = client_address
+                address_to_pid[client_address] = pid
+                message_queue[(client_address[0], client_address[1]+1)] = Queue.Queue()
+            else:
+                server_binding_addr = (str(ip), int(port)) 
+            pid_count = pid_count + 1 
 
+for i in range(pid_count):
+    pid_to_vector[i+1] = [0] * pid_count 
 
 lock = threading.Lock()
-server_pid = sys.argv[1]
-server_binding_addr = pid_to_address[server_pid]
 client_port_offset = 0 
 min_delay, max_delay = delay.strip().split(' ')
 server = server_init()
+inputs = [server]
+outputs = []
 listener = listener_thread(message_queue, address_to_pid)
 listener.start()
-client = client_thread(server, pid_to_address)
+client = client_thread(message_queue, pid_to_address)
 client.start()
-
-
-
-
-
-
-
-
